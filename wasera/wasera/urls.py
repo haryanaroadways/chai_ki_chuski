39dc184752f05ecd286fc8b678f082a0405780b7
manish <manish@localhost.localdomain>
2018-03-18 03:07:48 +0530

    second

--- wasera/wasera/urls.py
+++ wasera/wasera/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from register import views
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
 ]
